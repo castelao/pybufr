a7ab8a8efdc13534361417f53bad1acafcb12ef5
Gui <guilherme@castelao.net>
2008-03-19 16:05:25 +0000

    Description is now included on the class 3 also. Section 3 reader was moved from teste.py into the bufr.py

--- bufr.py
+++ bufr.py
@@ -97,12 +97,14 @@ class section1v4(IterableUserDict):
             self.data['localuse'] = self.f.read(n_missing)
         return
 
+
+
 ##############################################################################
 #### Descriptor tables
 ##############################################################################
 
 
-class descriptorstable(IterableUserDict):
+class DescriptorsTable(IterableUserDict):
     """
         This class should return the descriptor definitions on demand.
 
@@ -154,15 +156,317 @@ class descriptorstable(IterableUserDict):
             if F == 0:
                 #if Y not in descriptors[F][X]:
                 #    descriptors[F][X]={}
-                self.data[F][X][Y] = {'name':fields[1],'unit':fields[2],'scale':fields[3],'reference':fields[4],'bitwidth':fields[5]}
+                #self.data[F][X][Y] = {'mnemonic':fields[4],'unit':fields[5],'scale':fields[1],'reference':fields[2],'bitwidth':fields[3]}
+                self.data[F][X][Y] = {'descriptor':fields[0],'scale':fields[1],'reference':fields[2],'bitwidth':fields[3],'mnemonic':fields[4],'unit':fields[5],'description':fields[6]}
             elif F == 3:
                 # Temporary ugly solution
                 if re.search("[ABCD]",fields[2]):
-                    self.data[F][X][Y] = {'description':fields[1],'fields':[[d[0],int(d[2:4]),int(d[5:8])] for d in fields[2][:-1].split(' ')],'mnemonic':fields[3]}
+                    self.data[F][X][Y] = {'description':fields[1],'fields':[[d[0],int(d[2:4]),int(d[5:8])] for d in fields[2].split(' ')],'mnemonic':fields[3]}
                 else:
-                    self.data[F][X][Y] = {'description':fields[1],'fields':[[int(d[0]),int(d[2:4]),int(d[5:8])] for d in fields[2][:-1].split(' ')],'mnemonic':fields[3]}
+                    self.data[F][X][Y] = {'description':fields[1],'fields':[[int(d[0]),int(d[2:4]),int(d[5:8])] for d in fields[2].split(' ')],'mnemonic':fields[3]}
         return
 
+##############################################################################
+#### SECTION 3
+##############################################################################
+from UserDict import UserDict
+from UserDict import IterableUserDict
+
+descriptorstable = DescriptorsTable()
+
+#filename = './data/wmo_sarep.bufr'
+#f=open(filename,'r')
+#lixo=f.read(30)
+
+
+# Looks like a good first approach, but still needs:
+#    - Think about an array index. I believe is the best way to reference were to store the output data
+#    - Think about a rewind system for the walk. Maybe simply save a copy, of the data when the walk is called, and when the rewind is called, it only overload the copied data.
+#    - Still missing how to deal with the repetitions (class 1).
+
+#import itertools
+
+
+#class level0(UserList):
+#    def __init__(self):
+#        UserList.__init__(self)
+#   return
+
+class Descriptor0(UserDict):
+    #def __init__(self,data):
+    def __init__(self,F,X,Y):
+        self.F=F
+        self.X=X
+        self.Y=Y
+        #self.data=globals()['descriptorstable'][F][X][Y]
+        self.data=globals()['descriptorstable'][F,X,Y]
+        self.showed=False
+        self.repetitions=1
+        self.n=0
+        return
+    def walk(self):
+        if self.showed==False:
+            self.showed=True
+            return self
+        else:
+            self.showed=False
+            return
+    def reset(self):
+        self.showed=False
+        return
+#class WalkingList(UserList):
+#    #def __init__(self):
+#    def __init__(self,data=None):
+#        UserList.__init__(self)
+#   if data!=None:
+#            self.data=data
+#   self.i=-1
+#   print "Merda: i",self.i
+#   return
+#    def walk(self):
+#        self.i+=1
+#   if self.i<len(self):
+#       return self.data[self.i]
+#   return 
+
+
+class Descriptors(UserList):
+    def __init__(self,F=None,X=None,Y=None):
+    #def __init__(self):
+        UserList.__init__(self)
+        #self.data=WalkingList()
+        self.i=0
+        self.repetitions=1
+        self.n=0
+        if (F != None) & (X != None) & (Y != None):
+            self.append(F,X,Y)
+        self.dataind=-1
+        return
+    def load(self,descriptorslist):
+        """
+        """
+        i=0
+        while i<len(descriptorslist):
+            F,X,Y = descriptorslist[i]
+            #print "Processing: %s, %s, %s" % (F,X,Y)
+            #print F,X,Y,(globals()['descriptorstable'][F][X][Y])
+            if (F==0):
+                self.data.append(Descriptor0(F,X,Y))
+                self.dataind+=1
+                #print "self.dataind: ",self.dataind
+                self.data[-1]['index']=self.dataind
+                #print self.data[-1]
+            elif (F==1):
+                tmp=Descriptors()
+                tmp.dataind=self.dataind
+                tmp.F=F
+                tmp.X=X
+                tmp.Y=Y
+                if tmp.Y>0:
+                    sublist=descriptorslist[i+1:i+1+X]
+                    tmp.repetitions=Y
+                    i+=X
+                elif tmp.Y==0:
+                    sublist=descriptorslist[i+2:i+2+X]
+                    tmp.repfactor=Descriptor0(descriptorslist[i+1][0],descriptorslist[i+1][1],descriptorslist[i+1][2])
+                    tmp.repetitions=None
+                    i+=1+X
+                for f,x,y in sublist:
+                    if f==1:
+                        print "Library not ready for hierarcheal repetitions"
+                        self.data=None
+                        return
+                tmp.load(sublist)
+                self.dataind=tmp.dataind
+                self.data.append(tmp)
+            elif (F==3):
+                tmp=Descriptors()
+                tmp.dataind=self.dataind
+                tmp.F=F
+                tmp.X=X
+                tmp.Y=Y
+                tmp.load(globals()['descriptorstable'][F,X,Y]['fields'])
+                self.dataind=tmp.dataind
+                self.data.append(tmp)
+            i+=1
+        return
+    def walk(self):
+        #print "self.i: %s" % (self.i)
+        output=self.data[self.i].walk()
+        #print "output:",output
+        #print "i:",i
+        if (output == None):
+            #print "i: %s, len(data): %s" % (self.i,len(self.data))
+            if self.i<(len(self.data)-1):
+                self.i+=1
+                #self.n+=1
+                if (self.data[self.i].F == 1) & (self.data[self.i].Y == 0) & ((self.data[self.i].n == 0)):
+                    output = self.data[self.i].repfactor
+                else:
+                    output=self.data[self.i].walk()
+            #if self.n<self.repetitions:
+            #    self.i=0
+            #if (self.i<len(self.data)):
+            #    output=self.data[self.i].walk()
+            else:
+                self.n+=1
+                if (self.n<self.repetitions):
+                    self.reset()
+                    output=self.data[self.i].walk()
+                else:
+                    output=None
+        #else:
+        #    #self.i+=1
+        #    #print "I'm out"
+        return output
+    def reset(self):
+        """ Reset the counter
+        """
+        self.data[self.i].reset()
+        self.i=0
+        return
+#a=Descriptors()
+#a.load(descriptorslist)
+
+#    def append(self,F,X,Y):
+#   #self.i=0
+#   if (F==0):
+#       #self.data.append(WalkingList(globals()['descriptorstable'][F][X][Y]))
+#       #self.data.append(Descriptor0(globals()['descriptorstable'][F][X][Y]))
+#       #self.data.append(Descriptor0(F,X,Y))
+#       tmp=(Descriptor0(F,X,Y))
+#       #self.data.append((globals()['descriptorstable'][F][X][Y]))
+#        elif (F==1):
+#       # Replicator class
+#       # X is the number of the following fields that should be replicated
+#       # Y if > 0 is the number of repetitions
+#       #   if = 0 means a delayed replicaton, and a class 0 31 should be the following field.
+#       print "Hey!!! F=1!!!"
+#       #self.nfields=X
+#       tmp=Descriptors()
+#       tmp.F=F
+#       tmp.X=X
+#       tmp.Y=Y
+#       self.insiderepetition=True
+#       #if Y==0:
+#       #    # Need to read n on the next field
+#       #    pass
+#       #else:
+#       #    self.n=Y
+#   elif (F==3):
+#       tmp=Descriptors()
+#       tmp.F=F
+#       tmp.X=X
+#       tmp.Y=Y
+#       for f,x,y in globals()['descriptorstable'][F][X][Y]:
+#           print f,x,y
+#           #tmp.append(Descriptors(f,x,y))
+#           tmp.append(f,x,y)
+#       #self.data.append(tmp)
+#   if self.insiderepetition==True
+#       #self.tmp.append(tmp)
+#       #if len(self.tmp)==self.tmp.X:
+#       #    self.insiderepetition==False
+#       #   self.data.append(self.tmp)
+#       self.data[-1].append(tmp)
+#   else:
+#            self.data.append(tmp)
+#   return
+#    def parser(self,descriptorlist):
+#        for i,[F,X,Y] in enumerate(descriptorlist):
+#       print "i: %i" % i
+#       if F==0:
+#           #self.data.append(Descriptor0(globals()['descriptorstable'][F][X][Y]))
+#       self.append(F,X,Y)
+#            elif F==1:
+#           if Y>0:
+#           start=i+1
+#           end=i+1+X
+#                elif Y==0:
+#           start=i+2
+#           end=i+2+X
+#                tmp=Descriptors()
+#                tmp.parser(descriptorlist[start:end])
+#                print "tmp",tmp
+#       elif F==3:
+#           ##tmp=[]
+#           #tmp=Descriptors()
+#           #for f,x,y in globals()['descriptorstable'][F][X][Y]:
+#           #    #tmp.append(Descriptors(f,x,y))
+#           #    tmp.append(f,x,y)
+#       #self.data.append(tmp)
+#       self.append(F,X,Y)
+#        return
+#    def walk(self):
+#        output=self.data[self.i].walk()
+#   #print "output:",output
+#   #print "i:",i
+#   if (output == None):
+#       self.i+=1
+#       if (self.i<len(self.data)):
+#                output=self.data[self.i].walk()
+#            else:
+#           output=None
+#   return output
+
+# To test:
+#filename = '/home/castelao/work/projects/BUFR/others/bufr_000340/data/wmo_sarep.bufr'
+#f=open(filename,'r')
+#lixo=f.read(30)
+
+# x=Descriptor(3,1,1)
+# x.walk()
+# x.walk()
+
+
+#class Descriptors:
+#    def __init__(self,descriptors):
+#        self.descriptors=descriptors
+#        self.i=-1
+#        return
+#    def walk(self):
+#        self.i+=1
+#        return self.descriptors[self.i]
+
+
+
+class section3(IterableUserDict):
+    """A class to read the section 3 of a BUFR file
+    """
+    def __init__(self,f):
+        """
+        """
+        self.f = f
+        self.data={}
+        self.read()
+        return
+    def read(self):
+        self.data[3]={}
+        self.data[3]['sec3size'] = safe_unpack('>i',self.f.read(3))
+        self.data[3]['reserved'] = safe_unpack('>i', self.f.read(1))
+        self.data[3]['nsubsets'] = safe_unpack('>i', self.f.read(2))
+        self.data[3]['xxxx'] = safe_unpack('>i', self.f.read(1))
+
+        n_descriptors = (self.data[3]['sec3size']-7)/2
+        self.data[3]['descriptors']=Descriptors()
+        tmp=[]
+        for i in range(n_descriptors):
+            FX = safe_unpack('>i', self.f.read(1))
+            F = FX/64
+            X = FX%64
+            Y = safe_unpack('>i', self.f.read(1))
+            #sectiondata[3]['descriptors'].append([FX,F,X,Y])
+            #sectiondata[3]['descriptors'].append(F,X,Y)
+            tmp.append([F,X,Y])
+            #print F,X,Y
+            #if F == 0:
+            #    print descriptorstable[F][X][Y]
+
+            #Read the rest of the section non used bytes. Should be 0 or 1 bytes.
+            trash=self.f.read((self.data[3]['sec3size']-7)%2)
+
+            self.data[3]['descriptors'].load(tmp)
+        return
 ##############################################################################
 #### Unreviewed
 ##############################################################################
@@ -221,28 +525,6 @@ from UserDict import UserDict
 #        UserList.__init__(self)
 #	return
 
-class Descriptor0(UserDict):
-    #def __init__(self,data):
-    def __init__(self,F,X,Y):
-        self.F=F
-        self.X=X
-        self.Y=Y
-        self.data=globals()['descriptorstable'][F][X][Y]
-        self.showed=False
-        self.repetitions=1
-        self.n=0
-        return
-    def walk(self):
-        if self.showed==False:
-            self.showed=True
-            return self
-        else:
-            self.showed=False
-            return
-    def reset(self):
-        self.showed=False
-        return
-	
 #class WalkingList(UserList):
 #    #def __init__(self):
 #    def __init__(self,data=None):
@@ -257,105 +539,6 @@ class Descriptor0(UserDict):
 #	if self.i<len(self):
 #	    return self.data[self.i]
 #	return 
-
-class Descriptors(UserList):
-    def __init__(self,F=None,X=None,Y=None):
-    #def __init__(self):
-        UserList.__init__(self)
-        #self.data=WalkingList()
-        self.i=0
-        self.repetitions=1
-        self.n=0
-        if (F != None) & (X != None) & (Y != None):
-            self.append(F,X,Y)
-        self.dataind=-1
-        return
-    def load(self,descriptorslist):
-        """
-        """
-        i=0
-        while i<len(descriptorslist):
-            F,X,Y = descriptorslist[i] 
-            #print "Processing: %s, %s, %s" % (F,X,Y)
-            #print F,X,Y,(globals()['descriptorstable'][F][X][Y])
-            if (F==0):
-                self.data.append(Descriptor0(F,X,Y))
-                self.dataind+=1
-                print "self.dataind: ",self.dataind
-                self.data[-1]['index']=self.dataind
-                print self.data[-1]
-            elif (F==1):
-                tmp=Descriptors()
-                tmp.dataind=self.dataind
-                tmp.F=F
-                tmp.X=X
-                tmp.Y=Y
-                if tmp.Y>0:
-                    sublist=descriptorslist[i+1:i+1+X]
-                    tmp.repetitions=Y
-                    i+=X
-                elif tmp.Y==0:
-                    sublist=descriptorslist[i+2:i+2+X]
-                    tmp.repfactor=Descriptor0(descriptorslist[i+1][0],descriptorslist[i+1][1],descriptorslist[i+1][2])
-                    tmp.repetitions=None
-                    i+=1+X
-                for f,x,y in sublist:
-                    if f==1:
-                        print "Library not ready for hierarcheal repetitions"
-                        self.data=None
-                        return
-                tmp.load(sublist)
-                self.dataind=tmp.dataind
-                self.data.append(tmp)
-            elif (F==3):
-                tmp=Descriptors()
-                tmp.dataind=self.dataind
-                tmp.F=F
-                tmp.X=X
-                tmp.Y=Y
-                tmp.load(globals()['descriptorstable'][F][X][Y])
-                self.dataind=tmp.dataind
-                self.data.append(tmp)
-            i+=1
-        return
-    def walk(self):
-        #print "self.i: %s" % (self.i)
-        output=self.data[self.i].walk()
-        #print "output:",output
-        #print "i:",i
-        if (output == None):
-            #print "i: %s, len(data): %s" % (self.i,len(self.data))
-            if self.i<(len(self.data)-1):
-                self.i+=1
-                #self.n+=1
-                if (self.data[self.i].F == 1) & (self.data[self.i].Y == 0) & ((self.data[self.i].n == 0)):
-                    output = self.data[self.i].repfactor
-                else:
-                    output=self.data[self.i].walk()
-            #if self.n<self.repetitions:
-            #    self.i=0
-            #if (self.i<len(self.data)):
-            #    output=self.data[self.i].walk()
-            else:
-                self.n+=1
-                if (self.n<self.repetitions):
-                    self.reset()
-                    output=self.data[self.i].walk()
-                else:
-                    output=None
-        #else:
-        #    #self.i+=1
-        #    #print "I'm out"
-        return output
-    def reset(self):
-        """ Reset the counter
-        """
-        self.data[self.i].reset()
-        self.i=0
-        return
-
-#a=Descriptors()
-#a.load(descriptorslist)
 
 #    def append(self,F,X,Y):
 #	#self.i=0
